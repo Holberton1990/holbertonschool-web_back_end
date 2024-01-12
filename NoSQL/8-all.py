13c371cf4ae6f78736ef9aa84356a878779809e7
Holberton1990 <ersida9019@gmail.com>
2024-01-12 21:41:11 +0000

    NoSql

---
+++ NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+NoSQL
+'''
+
+
+def list_all(mongo_collection):
+    '''
+    function that lists all documents in a collection
+    '''
+    return mongo_collection.find()
+    
